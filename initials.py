bd8bf58598ca87051ee5627721809236e2036abc
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-04-01 09:29:50 -0700

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Anjanique Mackey
+# Mi fruta favorita es frambuesas
+print('  A   M   M')
+print(' A A  MM MM')
+print('A   A MM MM')
+print('AAAAA M M M')
+print('A   A M   M')
+print('A   A M   M')
+print('A   A M   M')
